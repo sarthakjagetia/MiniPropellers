fc28f1ef0eeeb26b72df803674d65295b7f74741
Sarthak <sarthakjagetia@gmail.com>
2021-01-23 18:42:48 -0500

    Adding drone sprite

--- app/src/main/java/com/example/minipropellers/GameView.kt
+++ app/src/main/java/com/example/minipropellers/GameView.kt
@@ -1,6 +1,9 @@
 package com.example.minipropellers
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 
@@ -30,6 +33,16 @@ class GameView(context: Context) : SurfaceView(context), SurfaceHolder.Callback 
                 exception.printStackTrace()
             }
             retry = false
+        }
+    }
+
+    override fun draw(canvas: Canvas?) {
+        super.draw(canvas)
+        if (canvas != null) {
+            canvas.drawColor(Color.WHITE)
+            val paint = Paint()
+            paint.color = Color.rgb(250, 0, 0)
+            canvas.drawRect(100F, 100F, 200F, 200F, paint)
         }
     }
 
